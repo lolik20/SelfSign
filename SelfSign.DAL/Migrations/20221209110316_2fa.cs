948409bdab03804b760d6126a2502470541b664e
Максим Максим <mailsteama230@mail.ru>
2022-12-09 14:03:18 +0300

    Migration 2fa

---
+++ SelfSign.DAL/Migrations/20221209110316_2fa.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SelfSign.DAL.Migrations
+{
+    public partial class _2fa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAuthenticated",
+                table: "Requests",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAuthenticated",
+                table: "Requests");
+        }
+    }
+}
